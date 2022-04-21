df4ad821828979ea1e58abdd5f1fe2c2ec88810a
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-04-21 16:39:41 -0700

    small change

--- src/Shape.java
+++ src/Shape.java
@@ -2,11 +2,9 @@ import java.awt.Color;
 import java.awt.Point;
 
 public interface Shape {
-    
     Color getColor();
     void setColor(Color color);
     double getArea();
     double getPerimeter();
     void translate(Point p);
-
 }
